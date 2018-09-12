4a3090f699fb9335a6d561e715e02883092c4225
Higo Soares do Lago <higo.lago@gmail.com>
2018-09-12 16:58:39 -0300

    Migration para dar update em um produto especifico

---
+++ src/Migrations/Version20180912195025.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20180912195025 extends AbstractMigration
+{
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('UPDATE produto SET tx_url_imagem_produto="images/camiseta-basica-masculina-hering.jpg;images/camiseta-hering-masculina.jpg" WHERE id_produto=1');
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
